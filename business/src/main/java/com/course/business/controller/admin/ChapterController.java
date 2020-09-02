fbebeb40ce118b292541a155306b3a02edd0e844
ManMian <1515911131@qq.com>
2020-09-02 21:29:05 +0800

    1、会员admin，所以重构，测试连接http://127.0.0.1:9002/business/admin/chapter

--- business/src/main/java/com/course/business/controller/admin/ChapterController.java
+++ business/src/main/java/com/course/business/controller/admin/ChapterController.java
@@ -1,4 +1,4 @@
-package com.course.business.controller;
+package com.course.business.controller.admin;
 
 //import com.course.server.domain.Chapter;
 //import com.course.server.service.ChapterService;
@@ -7,12 +7,14 @@ package com.course.business.controller;
 import com.course.server.pojo.Chapter;
 import com.course.server.service.ChapterService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.util.List;
 
 @RestController
+@RequestMapping("/admin/")
 public class ChapterController {
 
     @Resource
