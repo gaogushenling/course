86c7fcfc8cd4706a67f4b9f3095ca1374e53f7d7
ManMian <1515911131@qq.com>
2020-09-28 21:49:40 +0800

    1、依赖问题真的解决了吗？

--- business/src/main/java/com/course/business/controller/admin/ChapterController.java
+++ business/src/main/java/com/course/business/controller/admin/ChapterController.java
@@ -1,13 +1,12 @@
 package com.course.business.controller.admin;
 
-import com.course.server.dto.ChapterDto;
+import com.course.server.dto.PageDto;
 import com.course.server.service.ChapterService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.util.List;
 
 @RestController
 @RequestMapping("/admin/chapter/")
@@ -17,8 +16,9 @@ public class ChapterController {
     public ChapterService chapterService;
 
     @GetMapping(value = "list", produces = {"application/json;charset=UTF-8"})
-    public List<ChapterDto> list() {
-        return chapterService.list();
+    public PageDto list(PageDto pageDto) {
+        chapterService.list(pageDto);
+        return pageDto;
     }
 
 }
--- server/src/main/java/com/course/server/service/ChapterService.java
+++ server/src/main/java/com/course/server/service/ChapterService.java
@@ -1,10 +1,12 @@
 package com.course.server.service;
 
 import com.course.server.dto.ChapterDto;
+import com.course.server.dto.PageDto;
 import com.course.server.mapper.ChapterMapper;
 import com.course.server.domain.Chapter;
 import com.course.server.domain.ChapterExample;
 import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 import javax.annotation.Resource;
@@ -17,17 +19,26 @@ public class ChapterService {
     @Resource
     private ChapterMapper chapterMapper;
 
-    public List<ChapterDto> list() {
-        PageHelper.startPage(1,1);
+    public void list(PageDto pageDto) {
+        PageHelper.startPage(pageDto.getPage(),pageDto.getSize());
         ChapterExample chapterExample = new ChapterExample();
         List<Chapter> chapterList = chapterMapper.selectByExample(chapterExample);
-        List<ChapterDto> chapterDtoList = new ArrayList<ChapterDto>();
+
+        //PageInfo插件内置对象
+        PageInfo<Chapter> pageInfo = new PageInfo<>(chapterList);
+        //获取数据总行数
+        pageDto.setTotal(pageInfo.getTotal());
+
+        List<ChapterDto> chapterDtoList = new ArrayList<>();
         for (Chapter chapter : chapterList) {
             ChapterDto chapterDto = new ChapterDto();
             BeanUtils.copyProperties(chapter, chapterDto);
             chapterDtoList.add(chapterDto);
         }
-        return chapterDtoList;
+        //数据
+        pageDto.setList(chapterDtoList);
+
+//        return pageDto;前端传的也是pageDto，所以不返回，前端也可以拿到。说白了，就是在Controlller中返回
     };
 
 }
